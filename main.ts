b8eefaad3841b406d1f1acc77c102ca3239b7ff1
royyanbach <royyanbach@users.noreply.github.com>
2024-08-08 17:10:51 +0700

    Rename function

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 import * as functions from '@google-cloud/functions-framework';
 
-functions.http('getAll', (req, res) => {
+functions.http('getAllStockNews', (req, res) => {
  res.send(`Hello ${req.query.name || req.body.name || 'World'}!`);
 });
