6135d1f5366d0cb4534468f57b8af751d9ee2a0a
royyanbach <royyanbach@users.noreply.github.com>
2024-08-09 09:54:59 +0700

    Use esnext

---
+++ adapters/kontan.ts
@@ -0,0 +1,163 @@
+import fetch from 'node-fetch';
+import { parse } from 'node-html-parser';
+import pLimit from 'p-limit';
+import { padNumberToString } from '../utils';
+
+const ITEMS_PER_PAGE = 20;
+const CONCURRENCY = 5;
+
+const limit = pLimit(CONCURRENCY);
+
+export async function fetchNewsDetail(link: string) {
+  try {
+    const response = await fetch(link);
+    const body = await response.text();
+    const root = parse(body);
+    const articleBody = root.querySelectorAll('[itemprop="articleBody"] p');
+
+    if (!articleBody.length) {
+      throw new Error('No data found');
+    }
+
+    return articleBody.filter((item) => {
+      if (!item.text.trim()) return false;
+      if (item.querySelector('.track-selanjutnya')) return false;
+      if (item.querySelector('.track-menarikdibaca')) return false;
+      if (item.querySelector('.track-gnews')) return false;
+      return true;
+    }).map((item) => item.text.trim()).join(' ');
+  } catch (error) {
+    console.error(error);
+    return {
+      title: '',
+      content: '',
+    };
+  }
+}
+
+type NewsItem = {
+  link: string;
+  totalPage: number;
+  title: string;
+};
+
+export async function fetchNewsList({
+  date: _date,
+  month: _month,
+  year,
+  page,
+}: {
+  date: number;
+  month: number;
+  year: number;
+  page: number;
+}): Promise<NewsItem[]> {
+  let list: NewsItem[] = [];
+  try {
+    const date = padNumberToString(_date);
+    const month = padNumberToString(_month);
+    const offset = (page - 1) * ITEMS_PER_PAGE;
+    const offsetParam = offset ? `&per_page=${offset}` : '';
+    const response = await fetch(`https://www.kontan.co.id/search/indeks?kanal=investasi&tanggal=${date}&bulan=${month}&tahun=${year}&pos=indeks${offsetParam}`);
+
+    const body = await response.text();
+    const root = parse(body);
+    const articles = root.querySelectorAll('.list-berita ul li h1 a');
+
+    if (!articles.length) {
+      throw new Error('No data found');
+    }
+
+    const navigations = root.querySelectorAll('.cd-pagination li a');
+    const numberedNavigations = navigations.filter((item) => !isNaN(parseInt(item.text, 10)));
+
+    return articles.map((item) => ({
+      link: item.getAttribute('href') || '',
+      totalPage: numberedNavigations.length || 1,
+      title: item.text,
+    }));
+  } catch (error) {
+    console.error(error);
+    return list;
+  }
+}
+
+export async function fetchAllNewsByDate({
+  date,
+  month,
+  year,
+}: {
+  date: number;
+  month: number;
+  year: number;
+}): Promise<NewsItem[]> {
+  let list: NewsItem[] = [];
+  try {
+    const response = await fetchNewsList({
+      date,
+      month,
+      year,
+      page: 1,
+    });
+
+    list = response;
+    const totalPage = response[0].totalPage;
+
+    for (let i = 2; i <= totalPage; i += 1) {
+      const response = await fetchNewsList({
+        date,
+        month,
+        year,
+        page: i,
+      });
+
+      list = list.concat(response);
+    }
+
+    return list;
+  } catch (error) {
+    console.error(error);
+    return list;
+  }
+}
+
+type NewsItemWithDetail = NewsItem & {
+  content: string;
+};
+
+export async function fetchAllNewsByDateWithDetail({
+  date,
+  month,
+  year,
+}: {
+  date: number;
+  month: number;
+  year: number;
+}) {
+  let list: NewsItemWithDetail[] = [];
+  try {
+    const response = await fetchAllNewsByDate({
+      date,
+      month,
+      year,
+    });
+
+    // @ts-ignore
+    list = await Promise.all(
+      response.map((item) => limit(() => {
+        const url = new URL(item.link);
+        const page = 'all';
+        url.searchParams.set('page', page);
+        return fetchNewsDetail(`${url.toString()}`)
+      }))
+    );
+
+    return list.map((item, index) => ({
+      ...response[index],
+      content: item,
+    }));
+  } catch (error) {
+    console.error(error);
+    return list;
+  }
+}
--- main.ts
+++ main.ts
@@ -1,5 +1,28 @@
 import * as functions from '@google-cloud/functions-framework';
+import mongoDBClient from './service/mongodb';
+import { fetchAllNewsByDateWithDetail } from './adapters/kontan';
 
-functions.http('getAllStockNews', (req, res) => {
- res.send(`Hello ${req.query.name || req.body.name || 'World'}!`);
+functions.http('getAllStockNews', async (req, res) => {
+  try {
+    mongoDBClient.connect();
+    await mongoDBClient.db("admin").command({ ping: 1 });
+    const currentDate = new Date();
+    const news = await fetchAllNewsByDateWithDetail({
+      // date: currentDate.getDate(),
+      date: 4,
+      month: currentDate.getMonth() + 1,
+      year: currentDate.getFullYear(),
+    });
+
+    const db = mongoDBClient.db('stock-news');
+    const collection = db.collection('articles');
+    await collection.insertMany(news);
+    // res.send(`Hello ${req.query.name || req.body.name || 'World'}!`);
+    res.send('OK');
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  } finally {
+    await mongoDBClient.close();
+  }
 });
